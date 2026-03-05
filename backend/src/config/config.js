48bea8fe2ab9cd4d91a5f29e1b27d6918111f94b
Mano-Bharathii <manobharathi189@gmail.com>
2026-03-05 10:19:54 +0530

    Database and Config files changed.

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -7,4 +7,4 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Load .env
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+dotenv.config({ path: path.resolve(__dirname, '../../.env'), quiet: true });
